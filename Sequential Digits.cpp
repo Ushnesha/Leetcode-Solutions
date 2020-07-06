d2c10cdaaf59e799ccb0243b7fc3db79e9690b19
Ushnesha Daripa <sweetydaripa@gmail.com>
2020-07-07 00:30:46 +0530

    BFS

---
+++ Sequential Digits.cpp
@@ -0,0 +1,20 @@
+//BFS
+
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        queue<int> q;
+        vector<int> res;
+        for(int i=1;i<=9;i++)
+            q.push(i);
+        while(!q.empty()){
+            int f=q.front();
+            q.pop();
+            if(f>=low && f<=high) res.push_back(f);
+            if(f>high) break;
+            int num=f%10;
+            if(num<9) q.push(f*10+(num+1));
+        }
+        return res;
+    }
+};
